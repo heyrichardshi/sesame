eb1f33e3cf50406d918f3ae7af606f48d78c14b1
Richard <heyrichardshi@users.noreply.github.com>
2021-05-27 23:51:57 -0400

    div now shows last generated password

--- src/extension.js
+++ src/extension.js
@@ -1,10 +1,8 @@
 import { generatePhrase } from "./octothorpe.js";
 import "./sass/popup.scss";
 
-console.log(generatePhrase(16));
-
 function pushToHistory(phrase) {
-    chrome.storage.sync.get(["history", "historyLimit"], function(o) {
+    chrome.storage.sync.get(["history", "historyLimit"], function (o) {
         let history = [phrase].concat(o.history);
 
         if (history.length > o.historyLimit) {
@@ -17,15 +15,32 @@ function pushToHistory(phrase) {
     });
 }
 
-// function updateHistoryDiv() {
-//     chrome.storage.sync.get("history", ({ history }) => {
-//         console.log(history);
-//     });
-// }
+function getHistory() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.sync.get("history", ({ history }) => {
+            if (chrome.runtime.lastError) {
+                return reject(chrome.runtime.lastError);
+            }
+            resolve(history);
+        });
+    });
+}
+
+async function getLatestHistory() {
+    const history = await getHistory();
+    if (history.length) {
+        return Promise.resolve(history[0]);
+    } else {
+        return Promise.resolve(null);
+    }
+}
 
 let passwordField = document.getElementById("generatedPassword");
 document.getElementById("theEasyButton").addEventListener("click", async () => {
     let p = generatePhrase(16);
     passwordField.textContent = p;
     pushToHistory(p);
+});
+getLatestHistory().then((history) => {
+    passwordField.textContent = history || "***";
 });
